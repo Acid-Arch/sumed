35d5976330a2498c718b87407af3a7dc500e3829
Antonio D. Salcedo <antony9409@gmail.com>
2021-06-17 10:18:43 -0500

    nav bar

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,5 +1,14 @@
-import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import React, { useState } from "react";
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 
 const NavMenu = (props) => {
   const [collapsed, setCollapsed] = useState(true);
@@ -7,24 +16,30 @@ const NavMenu = (props) => {
   const toggleNavbar = () => setCollapsed(!collapsed);
 
   return (
-    <div>
-      <Navbar color="faded" light>
-        <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
+    <header>
+      {/* <Navbar color="faded" light> */}
+      <Navbar className="navbar-expand-sm navbar-toggleable-sm box-shadow mb-3">
+        <Container>
+          <NavbarBrand href="/" className="mr-auto">
+            reactstrap
+          </NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
       </Navbar>
-    </div>
+    </header>
   );
-}
-
+};
 
 export default NavMenu;
