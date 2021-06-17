ea64ee8e8c39817973ff0d6eaec001d4e844ab75
jorge <jorgebeltran227@hotmail.com>
2021-06-17 09:34:16 -0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from './components/Layout';
+import NavMenu from './components/NavMenu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout />
+      
+
+   
+    </div>
+  );
+}
+
+export default App;
